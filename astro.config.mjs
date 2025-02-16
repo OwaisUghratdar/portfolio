51809ada8860a2d66d984450a124eb2fddeb0028
Owais Ughratdar <owais.ughratdar@gmail.com>
2025-02-16 18:05:37 -0500

    prepare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://OwaisUghratdar.github.io',
+  base: '/portfolio', // Use your actual repo name
   vite: {
     plugins: [tailwindcss()]
   }
